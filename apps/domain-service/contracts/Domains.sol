30ea8e963d7098e347970c56bb18b071ab845275
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2022-03-23 15:33:33 -0400

    func to enable name registry

--- apps/domain-service/contracts/Domains.sol
+++ apps/domain-service/contracts/Domains.sol
@@ -5,7 +5,16 @@ pragma solidity ^0.8.10;
 import 'hardhat/console.sol';
 
 contract Domains {
+    // "mapping" data type to store names
+    mapping(string => address) public domains;
+
     constructor() {
         console.log('My domain contract');
+    }
+
+    // Adds names to mapping
+    function register(string calldata name) public {
+      domains[name] = msg.sender;
+      console.log("%s has registered a domain!", msg.sender);
     }
 }
