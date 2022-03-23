f0e2250d3d842bb417c6a923fff2651d7244deb0
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2022-03-23 19:07:31 -0400

    set root domain, enable domain purchases

--- apps/domain-service/contracts/Domains.sol
+++ apps/domain-service/contracts/Domains.sol
@@ -2,23 +2,46 @@
 
 pragma solidity ^0.8.10;
 
+import {StringUtils} from './libraries/StringUtils.sol';
 import 'hardhat/console.sol';
 
 contract Domains {
+    // Record root domain
+    string public tld;
+
     // "mapping" data type to store names
     mapping(string => address) public domains;
-
     // "mapping" to store values
     mapping(string => string) public records;
 
-    constructor() {
-        console.log('My domain contract');
+    // Make contract payable
+    constructor(string memory _tld) payable {
+        // Set root domain
+        tld = _tld;
+        console.log('%s name service deployed', _tld);
+    }
+
+    // Compute price of domain based on its length
+    function price(string calldata name) public pure returns (uint256) {
+        uint256 len = StringUtils.strlen(name);
+        require(len > 0);
+        if (len == 3) {
+            // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals).
+            return 5 * 10**17;
+        } else if (len == 4) {
+            return 3 * 10**17; // 0.3 Matic
+        } else {
+            return 1 * 10**17;
+        }
     }
 
     // Adds names to mapping
-    function register(string calldata name) public {
+    function register(string calldata name) public payable {
         // Check if name is unregistered
         require(domains[name] == address(0));
+        // Check if enough Matic was paid in transaction
+        require(msg.value >= _price, 'Not enough Matic paid');
+
         domains[name] = msg.sender;
         console.log('%s has registered a domain!', msg.sender);
     }
@@ -36,7 +59,11 @@ contract Domains {
     }
 
     // Get record for registered name
-    function getRecord(string calldata name) public view returns(string memory) {
-      return records[name];
+    function getRecord(string calldata name)
+        public
+        view
+        returns (string memory)
+    {
+        return records[name];
     }
 }
