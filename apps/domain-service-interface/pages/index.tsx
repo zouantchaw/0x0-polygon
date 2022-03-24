e3f2e3cf91bfd518a41b49da387a99b1230a7547
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2022-03-24 05:19:22 -0400

    contract interaction

--- apps/domain-service-interface/pages/index.tsx
+++ apps/domain-service-interface/pages/index.tsx
@@ -1,8 +1,15 @@
 import { useEffect, useState } from 'react';
 import styles from './index.module.css';
+import { ethers } from 'ethers';
+
+const tld = '.charaktor';
+const CONTRACT_ADDRESS = '0x54e535C10D301Db7425Aa39C6F52bcd6DE1e8023';
 
 export function Index() {
   const [currentAccount, setCurrentAccount] = useState('');
+
+  const [domain, setDomain] = useState('');
+  const [record, setRecord] = useState('');
 
   // Implement your connectWallet method here
   const connectWallet = async () => {
@@ -51,6 +58,64 @@ export function Index() {
     }
   };
 
+  const mintDomain = async () => {
+    // Don't run if the domain is empty
+    if (!domain) {
+      return;
+    }
+    // Alert the user if the domain is too short
+    if (domain.length < 3) {
+      alert('Domain must be at least 3 characters long');
+      return;
+    }
+    // Calculate price based on length of domain (change this to match your contract)
+    // 3 chars = 0.5 MATIC, 4 chars = 0.3 MATIC, 5 or more = 0.1 MATIC
+    const price =
+      domain.length === 3 ? '0.5' : domain.length === 4 ? '0.3' : '0.1';
+    console.log('Minting domain', domain, 'with price', price);
+    try {
+      const { ethereum } = window;
+      if (ethereum) {
+        const provider = new ethers.providers.Web3Provider(ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+          CONTRACT_ADDRESS,
+          contractAbi.abi,
+          signer
+        );
+
+        console.log('Going to pop wallet now to pay gas...');
+        let tx = await contract.register(domain, {
+          value: ethers.utils.parseEther(price),
+        });
+        // Wait for the transaction to be mined
+        const receipt = await tx.wait();
+
+        // Check if the transaction was successfully completed
+        if (receipt.status === 1) {
+          console.log(
+            'Domain minted! https://mumbai.polygonscan.com/tx/' + tx.hash
+          );
+
+          // Set the record for the domain
+          tx = await contract.setRecord(domain, record);
+          await tx.wait();
+
+          console.log(
+            'Record set! https://mumbai.polygonscan.com/tx/' + tx.hash
+          );
+
+          setRecord('');
+          setDomain('');
+        } else {
+          alert('Transaction failed! Please try again');
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   // If wallet is not connected, render connect wallet button
   const renderNotConnectedContainer = () => (
     <div className="connect-wallet-container">
@@ -58,11 +123,51 @@ export function Index() {
         src="https://www.charaktor.com/_next/static/images/banner-product-img-31f1eca34960fcd44c8e9e25f381486f.png"
         alt="Charaktor png"
       />
-      <button onClick={connectWallet} className="cta-button connect-wallet-button">
+      <button
+        onClick={connectWallet}
+        className="cta-button connect-wallet-button"
+      >
         Connect Wallet
       </button>
     </div>
   );
+
+  // Form to enter domain name and data
+  const renderInputForm = () => {
+    return (
+      <div className="form-container">
+        <div className="first-row">
+          <input
+            type="text"
+            value={domain}
+            placeholder="domain"
+            onChange={(e) => setDomain(e.target.value)}
+          />
+          <p className="tld"> {tld} </p>
+        </div>
+
+        <input
+          type="text"
+          value={record}
+          placeholder="whats ur favorite metaverse"
+          onChange={(e) => setRecord(e.target.value)}
+        />
+
+        <div className="button-container">
+				<button className='cta-button mint-button' onClick={mintDomain}>
+					Mint
+				</button> 
+          <button
+            className="cta-button mint-button"
+            disabled={null}
+            onClick={null}
+          >
+            Set data
+          </button>
+        </div>
+      </div>
+    );
+  };
 
   useEffect(() => {
     checkIfWalletIsConnected();
@@ -80,7 +185,8 @@ export function Index() {
           </header>
         </div>
 
-				{!currentAccount && renderNotConnectedContainer()}
+        {!currentAccount && renderNotConnectedContainer()}
+        {currentAccount && renderInputForm()}
 
         <div className="footer-container"></div>
       </div>
