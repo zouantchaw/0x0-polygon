d9734806f57a112722c399d9d7e6487b691e6f2f
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2022-03-27 15:35:03 -0400

    hide firebase keys

---
+++ apps/forum/firebaseConfig.ts
@@ -0,0 +1,11 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// web app's Firebase configuration
+import { firebaseConfig } from "./fbConfig";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
