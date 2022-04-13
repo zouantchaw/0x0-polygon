9667fdae4a2350b3e12bcd0997f5d6735ee2bcb5
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2022-04-13 12:19:08 -0400

    inject react-query-client

--- apps/domain-service-interface/pages/_app.tsx
+++ apps/domain-service-interface/pages/_app.tsx
@@ -3,8 +3,27 @@ import Head from 'next/head';
 import '../styles/global.css'
 import { Provider as WagmiProvider } from 'wagmi';
 import { providers } from 'ethers';
+import { QueryClient, QueryClientProvider, QueryCache } from 'react-query';
+import { Toaster, toast } from 'react-hot-toast';
+
 
 const provider = providers.getDefaultProvider('https://polygon-mumbai.g.alchemy.com/v2/Mjq0Kt-Ss5lzr2ZZ1uyFU5d0JIujPRw1');
+
+// Create a react-query client
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+  queryCache: new QueryCache({
+    onError: () => {
+      toast.error(
+        'Network Error: Ensure Metamask is connected to the same network that your contract is deployed to.'
+      );
+    },
+  }),
+});
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
@@ -14,9 +33,12 @@ function CustomApp({ Component, pageProps }: AppProps) {
       </Head>
       {/* Inject Wagmi provider */}
       <WagmiProvider autoConnect provider={provider}>
-        <main className="app">
-          <Component {...pageProps} />
-        </main>
+        {/* Inject react-query client */}
+        <QueryClientProvider client={queryClient}>
+          <main className="app">
+            <Component {...pageProps} />
+          </main>
+        </QueryClientProvider>
       </WagmiProvider>
     </>
   );
